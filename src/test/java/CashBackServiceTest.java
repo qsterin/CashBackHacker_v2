5564d5b47a5da3d1f9c7b3c8c66c6a23ad09854e
 Eвгений Костерин <qsterin@gmail.com>
2022-10-11 13:34:50 +0300

    initial junit4

--- src/test/java/CashBackServiceTest.java
+++ src/test/java/CashBackServiceTest.java
@@ -5,47 +5,47 @@ import static org.testng.Assert.*;
 public class CashBackServiceTest {
 
     @Test
-    void OverAThousand() {
+    public void OverAThousand() {
         int amount = 1850;
         int expected = 150;
         CashBackService service = new CashBackService();
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void Thousand() {
+    public void Thousand() {
         int amount = 1000;
         int expected = 0;
         CashBackService service = new CashBackService();
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void LessThanAThousand() {
+    public void LessThanAThousand() {
         int amount = 851;
         int expected = 149;
         CashBackService service = new CashBackService();
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void Null() {
+    public void Null() {
         int amount = 0;
         int expected = 1000;
         CashBackService service = new CashBackService();
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void One() {
+    public void One() {
         int amount = 999;
         int expected = 1;
         CashBackService service = new CashBackService();
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
